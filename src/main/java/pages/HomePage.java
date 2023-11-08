b68df0cd158d68ff6788fc3ca4de5f0790573ef3
targon97 <targon97@gmail.com>
2023-11-08 20:30:08 +0100

    xyz

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,16 +1,11 @@
 package pages;
 
-<<<<<<< HEAD
-=======
 import org.openqa.selenium.By;
->>>>>>> origin/costam-z-sda
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     WebDriver driver;
 
-<<<<<<< HEAD
-=======
     public HomePage(WebDriver driver) {
         this.driver = driver;
 
@@ -21,5 +16,4 @@ public class HomePage {
     public void clickLogin() {
         driver.findElement(loginButton).click();
     }
->>>>>>> origin/costam-z-sda
 }
