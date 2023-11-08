b33a1c83916d8373c14ddd8ddc63561e5d9def46
targon97 <targon97@gmail.com>
2023-11-08 18:54:36 +0100

    xyz

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,19 +1,8 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
     WebDriver driver;
 
-    public HomePage(WebDriver driver) {
-        this.driver = driver;
-
-    }
-
-    By loginButton = By.id("login");
-
-    public void clickLogin() {
-        driver.findElement(loginButton).click();
-    }
 }
